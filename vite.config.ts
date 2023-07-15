a4021d0e000c0fed5c181fbde3bfb5c388de141f
minl <minL.175@qq.com>
2023-07-15 10:27:44 +0800

    build: 项目搭建
    集成editorconfig配置，为不同 IDE
    编辑器上处理同一项目的多个开发人员维护一致的编码风格；集成prettier工具，对代码进行统一的格式化；集成ESLint,发现并修复代码问题；集成husky,在执行 `git commit
    `命令的时候对其进行校验，如果不符合eslint规范，那么自动通过规范进行修复；集成Commitizen，cz-conventional-changelog，规范代码提交风格；集成commitlint，限制不规范提交；集成Element-Plus；安装unplugin-vue-components和unplugin-auto-import插件，对vue相关函数以及Element-Plus相关函数和组件进行自动导入；集成Vue
    Router,Pinia;

---
+++ vite.config.ts
@@ -0,0 +1,45 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({
+      // Auto import functions from Vue, e.g. ref, reactive, toRef...
+      // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+      imports: ["vue"],
+
+      resolvers: [
+        // Auto import functions from Element Plus, e.g. ElMessage, ElMessageBox... (with style)
+        // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+        ElementPlusResolver(),
+      ]
+    }),
+    Components({
+      resolvers: [
+        // Auto register Element Plus components
+        // 自动导入 Element Plus 组件
+        ElementPlusResolver(),
+      ]
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url))
+    }
+  },
+  css: {
+    // 预处理器配置项
+    preprocessorOptions: {
+      less: {
+        math: "always"
+      }
+    }
+  }
+});
